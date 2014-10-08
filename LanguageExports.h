a15a31a8b67960720ee3e72db27c83888cac2982
Brandon-T <JustBrandonT@Gmail.com>
2014-10-08 18:48:53 -0400

    Update to support PascalScript.

--- LanguageExports.h
+++ LanguageExports.h
@@ -27,59 +27,25 @@
 
 static const char* PascalExports[] =
 {
-    "AcceptSocket", "Function Pro_AcceptSocket(ssl_info, ssl_client_info: ^SSLSocket): Boolean;",
-    "BindSocket", "Function Pro_BindSocket(ssl_info: ^SSLSocket): Boolean;",
-    "BytesPendingSocket", "Function Pro_BytesPendingSocket(ssl_info: ^SSLSocket): Integer;",
-    "CreateSocket", "Function Pro_CreateSocket(ssl_info: ^SSLSocket): Boolean;",
-    "ConnectSocket", "Function Pro_ConnectSocket(ssl_info: ^SSLSocket): Boolean;",
-    "ListenSocket", "Function Pro_ListenSocket(ssl_info: ^SSLSocket): Boolean;",
-    "SetBlockingSocket", "Function Pro_SetBlockingSocket(ssl_info: ^SSLSocket): Boolean;",
-    "SetTimeoutSocket", "Function Pro_SetTimeoutSocket(ssl_info: ^SSLSocket): Boolean;",
-    "SelectSocket", "Function Pro_SelectSocket(ssl_info: ^SSLSocket; Read: Boolean): Integer;",
-    "CloseSocket", "Function Pro_CloseSocket(ssl_info: ^SSLSocket): Boolean;",
-    "FreeSocket", "Function Pro_FreeSocket(ssl_info: ^SSLSocket): Boolean;",
-    "ReadSocket", "Function Pro_ReadSocket(ssl_info: ^SSLSocket; Buffer: ^Char; Size: Cardinal): Integer;",
-    "WriteSocket", "Function Pro_WriteSocket(ssl_info: ^SSLSocket; Buffer: String; Size: Cardinal): Integer;"
+    "AcceptSocket", "Function Pro_AcceptSocket(var ssl_info, ssl_client_info: SSLSocket): Boolean;",
+    "BindSocket", "Function Pro_BindSocket(var ssl_info: SSLSocket): Boolean;",
+    "BytesPendingSocket", "Function Pro_BytesPendingSocket(var ssl_info: SSLSocket): Integer;",
+    "CreateSocket", "Function Pro_CreateSocket(var ssl_info: SSLSocket): Boolean;",
+    "ConnectSocket", "Function Pro_ConnectSocket(var ssl_info: SSLSocket): Boolean;",
+    "ListenSocket", "Function Pro_ListenSocket(var ssl_info: SSLSocket): Boolean;",
+    "SetBlockingSocket", "Function Pro_SetBlockingSocket(var ssl_info: SSLSocket): Boolean;",
+    "SetTimeoutSocket", "Function Pro_SetTimeoutSocket(var ssl_info: SSLSocket): Boolean;",
+    "SelectSocket", "Function Pro_SelectSocket(var ssl_info: SSLSocket; Read: Boolean): Integer;",
+    "CloseSocket", "Function Pro_CloseSocket(var ssl_info: SSLSocket): Boolean;",
+    "FreeSocket", "Function Pro_FreeSocket(var ssl_info: SSLSocket): Boolean;",
+    "ReadSocket", "Function Pro_ReadSocket(var ssl_info: SSLSocket; Buffer: PChar; Size: Cardinal): Integer;",
+    "WriteSocket", "Function Pro_WriteSocket(var ssl_info: SSLSocket; Buffer: String; Size: Cardinal): Integer;"
 };
 
 static const char* PascalTypes[] =
 {
-	"SSLSocketType", "(TLS1_CLIENT_METHOD,\
-								TLS1_SERVER_METHOD,\
-								TLS11_CLIENT_METHOD,\
-								TLS11_SERVER_METHOD,\
-								SSL2_CLIENT_METHOD,\
-								SSL2_SERVER_METHOD,\
-								SSL3_CLIENT_METHOD,\
-								SSL3_SERVER_METHOD,\
-								SSL23_CLIENT_METHOD,\
-								SSL23_SERVER_METHOD);",
-
-	#ifndef x86_64
-	"SSLSocket", "packed record\
-							sock: Cardinal;\
-							ssl: Cardinal;\
-							ctx: Cardinal;\
-							address: PChar;\
-							socktype: SSLSocketType;\
-							timeout: Cardinal;\
-							port: Word;\
-							connected: Boolean;\
-							blockmode: Boolean;\
-						 end;",
-	#else
-	"SSLSocket", "packed record\
-							sock: Cardinal;\
-							ssl: Int64;\
-							ctx: Int64;\
-							address: PChar64;\
-							socktype: SSLSocketType;\
-							timeout: Cardinal;\
-							port: Word;\
-							connected: Boolean;\
-							blockmode: Boolean;\
-						end;"
-	#endif
+	"SSLSocketType", "(TLS1_CLIENT_METHOD, TLS1_SERVER_METHOD, TLS11_CLIENT_METHOD, TLS11_SERVER_METHOD, SSL2_CLIENT_METHOD, SSL2_SERVER_METHOD, SSL3_CLIENT_METHOD, SSL3_SERVER_METHOD, SSL23_CLIENT_METHOD, SSL23_SERVER_METHOD);",
+	"SSLSocket", "{$IFNDEF LAPE}record sock: Cardinal; ssl: Cardinal; ctx: Cardinal; address: PChar; socktype: Integer; timeout: Cardinal; port: Word; connected: Boolean; blockmode: Boolean; end;{$ELSE}packed record sock: Cardinal; ssl: Cardinal; ctx: Cardinal; address: PChar; socktype: SSLSocketType; timeout: Cardinal; port: Word; connected: Boolean; blockmode: Boolean; end;{$ENDIF}"
 };
 
 static const long int PascalExportCount = sizeof(PascalExports) / (sizeof(PascalExports[0]) * 2);
